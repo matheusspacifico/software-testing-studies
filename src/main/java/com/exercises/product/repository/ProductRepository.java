91df70a3adb8bf85ba394bcb5a7f37b0ce5814a1
matheusspacifico <matheuspacifico44@gmail.com>
2025-09-04 16:33:36 -0300

    refactor: change repository methods

--- src/main/java/com/exercises/product/repository/ProductRepository.java
+++ src/main/java/com/exercises/product/repository/ProductRepository.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public interface ProductRepository {
     Optional<Product> findById(Long id);
-    void save(Product product);
+    Long findLastId();
+    void saveOrUpdate(Product product);
     void delete(Product product);
-    void update(Product product);
 }
