747155e49c82c7ada1abd34e85ae9c4c647fdd66
matheusspacifico <matheuspacifico44@gmail.com>
2025-09-05 20:24:20 -0300

    fix: sum one to the last id when creating a new product in factory

--- src/test/java/com/exercises/product/usecases/UpdateProductUseCaseTest.java
+++ src/test/java/com/exercises/product/usecases/UpdateProductUseCaseTest.java
@@ -1,6 +1,7 @@
 package com.exercises.product.usecases;
 
 import com.exercises.product.domain.Product;
+import com.exercises.product.domain.ProductFactory;
 import com.exercises.product.repository.ProductRepository;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,16 +9,28 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UpdateProductUseCaseTest {
+
+    private ProductFactory factory;
 
     @Mock
     private ProductRepository repository;
 
     @InjectMocks
     private UpdateProductUseCase sut;
+
+    @BeforeEach
+    void setup() {
+        factory = new ProductFactory(repository);
+    }
 
     @Nested
     @DisplayName("When the input is null or blank")
@@ -27,6 +40,23 @@ public class UpdateProductUseCaseTest {
         @DisplayName("Should throw IllegalArgumentException when the input product object is null")
         void shouldThrowExceptionWhenInputIsNull() {
             assertThrows(IllegalArgumentException.class, () -> sut.update(null));
+        }
+    }
+
+    @Nested
+    @DisplayName("When the input fails pre-conditions")
+    class InvalidEntries {
+
+        @Test
+        @DisplayName("Should throw NoSuchElementException when the product was not found")
+        void shouldThrowExceptionWhenProductIsNotFound() {
+            Product product = factory.createProductFaker();
+            System.out.println(product.toString());
+
+            when(repository.findById(product.getId())).thenReturn(Optional.empty());
+
+            assertThrows(NoSuchElementException.class, () -> sut.update(product));
+            verify(repository).findById(product.getId());
         }
     }
 }
