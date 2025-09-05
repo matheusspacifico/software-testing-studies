665a4683038f1833b802a8cfa6822e16fe72a038
matheusspacifico <matheuspacifico44@gmail.com>
2025-09-05 20:22:49 -0300

    feat: add overridden to string method to product

--- src/main/java/com/exercises/product/domain/Product.java
+++ src/main/java/com/exercises/product/domain/Product.java
@@ -78,4 +78,16 @@ public class Product {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", category=" + category +
+                '}';
+    }
 }
