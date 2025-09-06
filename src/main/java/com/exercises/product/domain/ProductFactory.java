f850cbbef6fcb37a7306d67ae2e46a3ded9dd0b7
matheusspacifico <matheuspacifico44@gmail.com>
2025-09-05 21:42:37 -0300

    feat: add all missing set methods for product factory

--- src/main/java/com/exercises/product/domain/ProductFactory.java
+++ src/main/java/com/exercises/product/domain/ProductFactory.java
@@ -39,4 +39,55 @@ public class ProductFactory {
 
         return new Product(id, f.leagueOfLegends().champion(), f.leagueOfLegends().quote(), f.number().randomDouble(2, 1, 10000), f.number().numberBetween(1, 100), c.get(randomIndex));
     }
+
+    public Product createProductFakerSetName(String name) {
+        Long lastId = repository.findLastId();
+        Faker f = new Faker();
+        Random r = new Random();
+
+        List<Category> c = List.of(Category.values());
+        int randomIndex = r.nextInt(c.size());
+
+        return new Product(lastId + 1, name, f.leagueOfLegends().quote(), f.number().randomDouble(2, 1, 10000), f.number().numberBetween(1, 100), c.get(randomIndex));
+    }
+
+    public Product createProductFakerSetDescription(String description) {
+        Long lastId = repository.findLastId();
+        Faker f = new Faker();
+        Random r = new Random();
+
+        List<Category> c = List.of(Category.values());
+        int randomIndex = r.nextInt(c.size());
+
+        return new Product(lastId + 1, f.leagueOfLegends().champion(), description, f.number().randomDouble(2, 1, 10000), f.number().numberBetween(1, 100), c.get(randomIndex));
+    }
+
+    public Product createProductFakerSetPrice(Double price) {
+        Long lastId = repository.findLastId();
+        Faker f = new Faker();
+        Random r = new Random();
+
+        List<Category> c = List.of(Category.values());
+        int randomIndex = r.nextInt(c.size());
+
+        return new Product(lastId + 1, f.leagueOfLegends().champion(), f.leagueOfLegends().quote(), price, f.number().numberBetween(1, 100), c.get(randomIndex));
+    }
+
+    public Product createProductFakerSetQuantity(Integer quantity) {
+        Long lastId = repository.findLastId();
+        Faker f = new Faker();
+        Random r = new Random();
+
+        List<Category> c = List.of(Category.values());
+        int randomIndex = r.nextInt(c.size());
+
+        return new Product(lastId + 1, f.leagueOfLegends().champion(), f.leagueOfLegends().quote(), f.number().randomDouble(2, 1, 10000), quantity, c.get(randomIndex));
+    }
+
+    public Product createProductFakerSetCategory(Category category) {
+        Long lastId = repository.findLastId();
+        Faker f = new Faker();
+
+        return new Product(lastId + 1, f.leagueOfLegends().champion(), f.leagueOfLegends().quote(), f.number().randomDouble(2, 1, 10000), f.number().numberBetween(1, 100), category);
+    }
 }
