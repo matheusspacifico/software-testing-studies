141e087869064be1fdca2db17b0bdd060fe8377c
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-11 18:20:55 -0300

    feat: implement a similar version of java linked list for testing studies

---
+++ src/main/java/com/exercises/mylinkedlist/MyLinkedList.java
@@ -0,0 +1,75 @@
+package com.exercises.mylinkedlist;
+
+public class MyLinkedList<E> {
+    private Node<E> first;
+    private Node<E> last;
+    private int size = 0;
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    public void add(E e) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, e, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+
+    public boolean remove(Object o) {
+        if (o == null) {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (x.item == null) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        } else {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (o.equals(x.item)) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void unlink(Node<E> x) {
+        final Node<E> next = x.next;
+        final Node<E> prev = x.prev;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+
+        x.item = null;
+        size--;
+    }
+
+    public int size() {
+        return size;
+    }
+}
