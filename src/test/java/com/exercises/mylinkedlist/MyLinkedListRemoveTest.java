2fe2b8d3cc1d23285ab9253eddfce8e6bc28a283
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-12 16:41:46 -0300

    test: add "criterio de todas arestas" tests for my linked list remove method

--- src/test/java/com/exercises/mylinkedlist/MyLinkedListRemoveTest.java
+++ src/test/java/com/exercises/mylinkedlist/MyLinkedListRemoveTest.java
@@ -43,4 +43,47 @@ public class MyLinkedListRemoveTest {
             Assertions.assertEquals(2, sut.size());
         }
     }
+
+    @Nested
+    @Tag("MyLinkedListRemoveTodasArestas")
+    class CriterioTodasArestas {
+
+        @Test
+        void ShouldRemoveNonNullElement() {
+            sut.add("A");
+            sut.add("B");
+
+            Assertions.assertTrue(sut.remove("B"));
+            Assertions.assertEquals(1, sut.size());
+        }
+
+        @Test
+        void shouldRemoveNullElement() {
+            sut.add("A");
+            sut.add(null);
+
+            Assertions.assertTrue(sut.remove(null));
+            Assertions.assertEquals(1, sut.size());
+        }
+
+        @Test
+        void shouldNotRemoveWhenElementDoesntExist() {
+            sut.add("A");
+            sut.add("B");
+
+            Assertions.assertFalse(sut.remove("non-existent-element"));
+            Assertions.assertEquals(2, sut.size());
+        }
+
+        @Test
+        void shouldReturnFalseWhenRemovingNullFromListWithoutNulls() {
+            sut.add("A");
+            sut.add("B");
+
+            boolean result = sut.remove(null);
+
+            Assertions.assertFalse(result);
+            Assertions.assertEquals(2, sut.size());
+        }
+    }
 }
