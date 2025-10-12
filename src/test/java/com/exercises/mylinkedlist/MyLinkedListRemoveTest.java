b66c4d2c4c99e81cd80bf6d4659092459cc25f2f
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-12 16:11:46 -0300

    test: add "criterio de todos nos" test for my linked list remove method

---
+++ src/test/java/com/exercises/mylinkedlist/MyLinkedListRemoveTest.java
@@ -0,0 +1,46 @@
+package com.exercises.mylinkedlist;
+
+import org.junit.jupiter.api.*;
+
+@Tag("UnitTest")
+public class MyLinkedListRemoveTest {
+
+    private MyLinkedList<String> sut;
+
+    @BeforeEach
+    void setup() {
+        sut = new MyLinkedList<>();
+    }
+
+    @Nested
+    @Tag("MyLinkedListRemoveTodosNos")
+    class CriterioTodosNos {
+
+        @Test
+        void ShouldRemoveNonNullElement() {
+            sut.add("A");
+            sut.add("B");
+
+            Assertions.assertTrue(sut.remove("B"));
+            Assertions.assertEquals(1, sut.size());
+        }
+
+        @Test
+        void shouldRemoveNullElement() {
+            sut.add("A");
+            sut.add(null);
+
+            Assertions.assertTrue(sut.remove(null));
+            Assertions.assertEquals(1, sut.size());
+        }
+
+        @Test
+        void shouldNotRemoveWhenElementDoesntExist() {
+            sut.add("A");
+            sut.add("B");
+
+            Assertions.assertFalse(sut.remove("non-existent-element"));
+            Assertions.assertEquals(2, sut.size());
+        }
+    }
+}
